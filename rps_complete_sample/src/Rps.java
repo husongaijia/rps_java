c6210c4eb85f178167558e097269d5e28253be2c
戸松 愛佳 <tomatsu.aika@jp.fujitsu.com>
2019-09-22 13:29:04 +0900

    complete_sampleの追加

--- rps_complete_sample/src/Rps.java
+++ rps_complete_sample/src/Rps.java
@@ -1,2 +1,11 @@
 public class Rps {
+    public String play(String p1, String p2) {
+        if (p2.equals(p1)){
+            return "tie";
+        }
+        if ((p1.equals("rock") && p2.equals("scissors")) || (p1.equals("paper") && p2.equals("rock")) || (p1.equals("scissors") && p2.equals("paper"))) {
+            return "p1Wins";
+        }
+        return "p2Wins";
+    }
 }
--- rps_complete_sample/test/RpsTest.java
+++ rps_complete_sample/test/RpsTest.java
@@ -1,10 +1,129 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class RpsTest {
+    private Rps rps;
+    private String p1;
+    private String p2;
+
+    @Before
+    public void setUp() {
+        rps = new Rps();
+    }
+
     @Test
-    public void trueはtrueである() {
-        assertEquals(true, true);
+    public void p1がrockでp2がpaperのときp2Winsを返す() {
+        p1 = "rock";
+        p2 = "paper";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "p2Wins");
+
+    }
+
+    @Test
+    public void p1がrockでp2がrockのときtieを返す() {
+        p1 = "rock";
+        p2 = "rock";
+
+        String result = rps.play(p1, p2);
+
+        assertEquals(result, "tie");
+
+    }
+
+    @Test
+    public void p1がrockでp2がscissorsのときp1Winsを返す() {
+        p1 = "rock";
+        p2 = "scissors";
+
+        String result = rps.play(p1, p2);
+
+        assertEquals(result, "p1Wins");
+
+    }
+
+    @Test
+    public void p1がpaperでp2がpaperのときtieを返す() {
+        p1 = "paper";
+        p2 = "paper";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "tie");
+
+    }
+
+    @Test
+    public void p1がpaperでp2がrockのときp1Winsを返す() {
+        p1 = "paper";
+        p2 = "rock";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "p1Wins");
+
+    }
+
+    @Test
+    public void p1がpaperでp2がscissorsのときp2Winsを返す() {
+        p1 = "paper";
+        p2 = "scissors";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "p2Wins");
+
+    }
+
+    @Test
+    public void p1がscissorsでp2がpaperのときp1Winsを返す() {
+        p1 = "scissors";
+        p2 = "paper";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "p1Wins");
+
+    }
+
+    @Test
+    public void p1がscissorsでp2がrockのときp2Winsを返す() {
+        p1 = "scissors";
+        p2 = "rock";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "p2Wins");
+
+    }
+
+    @Test
+    public void p1がscissorsでp2がscissorsのときtieを返す() {
+        p1 = "scissors";
+        p2 = "scissors";
+
+
+        String result = rps.play(p1, p2);
+
+
+        assertEquals(result, "tie");
+
     }
 }
