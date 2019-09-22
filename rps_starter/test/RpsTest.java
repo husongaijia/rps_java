fee2d92d1daec3a0c6aee717665a411ac1235ab4
戸松 愛佳 <tomatsu.aika@jp.fujitsu.com>
2019-09-22 14:21:02 +0900

    starterにTODOを追加

--- rps_starter/test/RpsTest.java
+++ rps_starter/test/RpsTest.java
@@ -3,10 +3,25 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RpsTest {
-    // TODO 1. テストを動かしてみよう
-    // TODO 2. テストが通るようにactualを書き換えよう
+    /*TODO 1  テスト(trueはtrueである)を見て考えよう　(expectedが予期する値・actualが実際の値)
+     TODO 1-1 テストを実行した結果の予測をペアで話してから実行する　RED
+     TODO 1-2 テストが通るようにactualを書き換える
+     TODO 1-3 テストを実行した結果の予測をペアで話してから実行する　GREEN
+     */
     @Test
     public void trueはtrueである() {
         assertEquals(true, false);
     }
+
+
+    /* TODO 2 RpsクラスのplayメソッドをTDDで開発しよう
+     TODO 2-1 テストメソッドを作成する
+     TODO 2-2 失敗するテストを書く　AAAの形で【Arrange(準備)・Act(実行)・Assert(検証)】
+     TODO 2-3 テストを実行した結果の予測をペアで話してから実行する　RED
+     TODO 2-4 テストを通す最小限の実装を行う
+     TODO 2-5 テストを実行した結果の予測をペアで話してから実行する　GREEN
+     TODO 2-6 実装コード・テストコードともにリファクタリングする　REFACTOR
+     TODO 2-7 TODO 2-1~2-6を繰り返す
+     */
+
 }
